c2d4c4d6d07d0b13e1c1865c438d2d784ecf2685
Tiansi Dong <tdong@uni-bonn.de>
2019-03-13 19:55:23 +0100

    update author name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='nball4tree',
+setup(name='nbgraph',
       version='0.1',
       description='nball for DAG embedding',
       url='http://github.com/gnodisnait/nbgraph',
-      author='tiansi dong',
-      author_email='tian1shi2@gmail.com',
+      author='gnod isnait',
+      author_email='gnodisnait@gmail.com',
       license='MIT',
       packages= find_packages(),
       package_data = {'': ['*.pickle'],},
